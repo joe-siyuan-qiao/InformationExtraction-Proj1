2ea3c76894198c5fb365eda65a32e17fd9e965f3
Siyuan Qiao <joe.siyuan.qiao@gmail.com>
2017-02-24 13:23:24 -0500

    aeiou

--- python/hmm.py
+++ python/hmm.py
@@ -18,11 +18,8 @@ class model:
             initial emission matrix
         '''
         self.trans_mat = init_trans_mat
-        # self.emiss_mat = init_emiss_mat
+        self.emiss_mat = init_emiss_mat
         self.state_num = len(self.trans_mat)
-        self.emiss_mat = []
-        for i in range(self.state_num):
-            self.emiss_mat.append(deepcopy(init_emiss_mat))
 
     def read(self, filename):
         '''
@@ -52,6 +49,8 @@ class model:
             self.trellis[-1].post = [
                 [0 for i in range(self.state_num)]
                 for j in range(self.state_num)]
+            self.trellis[-1].gamma = [
+                0 for i in range(self.state_num)]
             for state_idx in range(self.state_num):
                 self.trellis[-1].node.append(__object__())
                 self.trellis[-1].node[-1].alpha = 0.0
@@ -75,7 +74,7 @@ class model:
                 acc_alpha = 0.0
                 for old_idx in range(self.state_num):
                     acc_alpha = acc_alpha + self.trans_mat[old_idx][state_idx]*\
-                        self.emiss_mat[old_idx][state_idx][self.data[data_idx] - 1]*\
+                        self.emiss_mat[state_idx][self.data[data_idx] - 1]*\
                         self.trellis[data_idx - 1].node[old_idx].alpha
                 self.trellis[data_idx].node[state_idx].alpha = acc_alpha
             acc_alpha = 0.0
@@ -94,33 +93,29 @@ class model:
                 acc_beta = 0.0
                 for old_idx in range(self.state_num):
                     acc_beta = acc_beta + self.trans_mat[state_idx][old_idx]*\
-                        self.emiss_mat[state_idx][old_idx][self.data[data_idx + 1] - 1]*\
+                        self.emiss_mat[old_idx][self.data[data_idx + 1] - 1]*\
                         self.trellis[data_idx + 1].node[old_idx].beta
                 acc_beta /= self.trellis[data_idx].norm
                 self.trellis[data_idx].node[state_idx].beta = acc_beta
 
     def update(self):
+        # update transition matrix
         for data_idx in range(self.data_len):
             for left_idx in range(self.state_num):
                 for right_idx in range(self.state_num):
                     alpha = self.trellis[data_idx].node[left_idx].alpha
                     trans = self.trans_mat[left_idx][right_idx]
-                    emiss = self.emiss_mat[left_idx][right_idx][
+                    emiss = self.emiss_mat[right_idx][
                                 self.data[data_idx + 1] - 1]
                     beta = self.trellis[data_idx + 1].node[right_idx].beta
                     prod = alpha * trans * emiss * beta
                     self.trellis[data_idx].post[left_idx][right_idx] = prod
         acc_trans = [[.0 for j in range(self.state_num)]
                         for i in range(self.state_num)]
-        acc_emiss = [[[.0 for k in range(27)] for j in range(self.state_num)]
-                        for i in range(self.state_num)]
         for data_idx in range(self.data_len):
             for left_idx in range(self.state_num):
                 for right_idx in range(self.state_num):
                     acc_trans[left_idx][right_idx] += self.trellis[
-                            data_idx].post[left_idx][right_idx]
-                    acc_emiss[left_idx][right_idx][self.data[
-                        data_idx] - 1] += self.trellis[
                             data_idx].post[left_idx][right_idx]
         for left_idx in range(self.state_num):
             acc_prob = 0.0
@@ -128,14 +123,27 @@ class model:
                 acc_prob += acc_trans[left_idx][right_idx]
             for right_idx in range(self.state_num):
                 acc_trans[left_idx][right_idx] /= acc_prob
-        for left_idx in range(self.state_num):
-            for right_idx in range(self.state_num):
-                acc_prob = 0.0
-                for out_idx in range(27):
-                    acc_prob += acc_emiss[left_idx][right_idx][out_idx]
-                for out_idx in range(27):
-                    acc_emiss[left_idx][right_idx][out_idx] /= acc_prob
         self.trans_mat = acc_trans
+        # update output matrix
+        for data_idx in range(1, self.data_len + 1):
+            acc_gamma = 0.0
+            for state_idx in range(self.state_num):
+                alpha = self.trellis[data_idx].node[state_idx].alpha
+                beta = self.trellis[data_idx].node[state_idx].beta
+                prod = alpha * beta
+                acc_gamma += prod
+                self.trellis[data_idx].gamma[state_idx] = prod
+            for state_idx in range(self.state_num):
+                self.trellis[data_idx].gamma[state_idx] /= acc_gamma
+        acc_emiss = [[.0 for i in range(27)] for i in range(self.state_num)]
+        for state_idx in range(self.state_num):
+            acc_gamma = 0.0
+            for data_idx in range(1, self.data_len + 1):
+                acc_emiss[state_idx][self.data[data_idx] - 1] += self.trellis[
+                    data_idx].gamma[state_idx]
+                acc_gamma += self.trellis[data_idx].gamma[state_idx]
+            for i in range(27):
+                acc_emiss[state_idx][i] /= acc_gamma
         self.emiss_mat = acc_emiss
 
 
